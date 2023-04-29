4da69162ddefbadd9af78da587ff6be9292993e1
Patrick Meyer <patrick.meyer1992@gmail.com>
2023-04-30 00:08:31 +0200

    Update app

--- src/pages/1_Results.py
+++ src/pages/1_Results.py
@@ -21,18 +21,29 @@ st.set_page_config(page_title="Geo-'Paw'-Sitioning",
 
 #st.pyplot(plot_example())
 
-titanic = sns.load_dataset("titanic")
+df_berlin = pd.read_csv("D:/GitHub/Geo-paw-sitioning/res/data/Geo-paw-sitioningBerlin.csv").drop(columns = ["geometry", "City_Name", "City_Code", "index_right", "Neighborhood_FID", "District_Name"])
 
-fig = plt.figure(figsize=(10, 4))
-sns.countplot(x="class", data=titanic)
+corr = np.corrcoef(df_berlin)
+fig_berlin = plt.figure(figsize=(30,20))
+sns.heatmap(corr, 
+        xticklabels=df_berlin.columns,
+        yticklabels=df_berlin.columns)
 
-st.pyplot(fig)
 
+left_column, right_column = st.columns([1,1])
 
-titanic = sns.load_dataset("titanic")
+# Add a selectbox to the sidebar:
+city_choice2 = st.sidebar.selectbox(
+    key='city',
+    label='Choose City',
+    options=('Berlin', 'Bremen', 'Dresden', 'Frankfurt_am_Main', 'Köln')
+)
 
-fig2 = plt.figure(figsize=(10, 4))
-sns.countplot(x="class", data=titanic)
+with left_column:
+    if city_choice2 == 'Berlin':
+        st.pyplot(fig_berlin)
 
-st.pyplot(fig2)
+with right_column:
+    if city_choice2 == 'Berlin':
+        st.pyplot(fig_berlin)
 
